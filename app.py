b092a0116924fc9a3a8e77af6a2679ec2f84a286
Aichara <daichara@JP-KIT-M-00089.local>
2020-03-23 13:41:58 +0900

    Update

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import dash
+import flask
+import os
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash('COVID19 India Tracker', external_stylesheets=external_stylesheets)
 server = app.server
 app.config.suppress_callback_exceptions = True
 app.title = 'COVID19 India Tracker'
+
+
+@server.route('/')
+def favicon():
+    return flask.send_from_directory(os.path.join(server.root_path, 'static'),
+                                     'favicon.ico')
