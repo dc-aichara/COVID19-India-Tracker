817d7ccedc2de4f17abaff1f7bf49bb6471fd43f
dc-aichara <dcaak7@gmail.com>
2020-04-02 21:29:01 +0900

    updated css file

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import dash
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', "static/css/table.css"]
+external_stylesheets = ["static/css/table.css", 'https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash('COVID19 India Tracker', external_stylesheets=external_stylesheets)
 server = app.server
 app.config.suppress_callback_exceptions = True
