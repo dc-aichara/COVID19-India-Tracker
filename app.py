7f152ad8bc08e0fd7f92e07bc18d6cf482065cad
dc-aichara <dcaak7@gmail.com>
2020-04-12 16:39:43 +0900

    Updated style

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ external_stylesheets = ["static/css/table.css", 'https://codepen.io/chriddyp/pen
 app = dash.Dash('COVID19 India Tracker', external_stylesheets=external_stylesheets)
 server = app.server
 app.config.suppress_callback_exceptions = True
-app.title = 'COVID19 India Tracker'
+app.title = 'Coronavirus (COVID19) India Tracker'
 
